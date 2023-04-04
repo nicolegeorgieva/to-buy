a80b05a02d77a8e9b4bd9a3e6624d03d9048449a
Nicole <nicole.georgievva@gmail.com>
2023-04-04 18:36:08 +0300

    Project builds

--- app/src/main/java/com/tobuy/Application.kt
+++ app/src/main/java/com/tobuy/Application.kt
@@ -1,8 +1,8 @@
-package com.employees.base
+package com.tobuy
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class EmployeesApp : Application() {
+class MyApp : Application() {
 }
---
+++ app/src/main/java/com/tobuy/di/DatabaseModule.kt
@@ -0,0 +1,22 @@
+package com.tobuy.di
+
+import android.content.Context
+import com.tobuy.persistence.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.create(context)
+    }
+}
+
