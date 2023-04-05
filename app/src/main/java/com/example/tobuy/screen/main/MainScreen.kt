d074e2b48250f8ead8017b00f8f5c209e0e83384
Nicole <nicole.georgievva@gmail.com>
2023-04-05 14:54:17 +0300

    Improve UI, add a bottom sheet content

--- app/src/main/java/com/example/tobuy/screen/main/MainScreen.kt
+++ app/src/main/java/com/example/tobuy/screen/main/MainScreen.kt
@@ -3,23 +3,23 @@ package com.example.tobuy.screen.main
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.tobuy.MainActivity
 import com.example.tobuy.R
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen() {
@@ -29,14 +29,47 @@ fun MainScreen() {
     UI(state = state, onEvent = viewModel::onEvent)
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun UI(
     state: MainState,
     onEvent: (MainEvent) -> Unit
 ) {
-    val mainActivity = LocalContext.current as MainActivity
+    val bottomSheetState =
+        rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
 
-    Box() {
+    ModalBottomSheetLayout(
+        sheetContent = {
+            BottomSheetContent()
+        },
+        sheetShape = RoundedCornerShape(
+            topStart = 16.dp,
+            topEnd = 16.dp
+        ),
+        sheetBackgroundColor = Color.White,
+        sheetState = bottomSheetState,
+        scrimColor = Color.Black.copy(alpha = 0.6f),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        MainContent(bottomState = bottomSheetState)
+    }
+}
+
+@Composable
+fun BottomSheetContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
+        Text("Your bottom sheet content")
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MainContent(bottomState: ModalBottomSheetState) {
+    Box {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
@@ -80,13 +113,18 @@ private fun UI(
             }
         }
 
+        val coroutineScope = rememberCoroutineScope()
         Button(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .padding(24.dp)
                 .size(64.dp),
             shape = CircleShape,
-            onClick = { /*TODO*/ }
+            onClick = {
+                coroutineScope.launch {
+                    bottomState.show()
+                }
+            }
         ) {
             Text(
                 text = "+",
